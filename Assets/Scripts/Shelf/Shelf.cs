801efa5d05329fc6128e0d5c3252214edcd6ca26
Guenther Koessler <guenther.koessler@imendo.at>
2018-08-11 13:15:35 +0200

    Added shelf file

---
+++ Assets/Scripts/Shelf/Shelf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : MonoBehaviour {
+	
+	// How full the shelf is
+	public float Level { get; set; } = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
