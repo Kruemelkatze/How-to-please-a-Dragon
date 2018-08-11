e043bd029d43f9d9cde1fbe31c111fedc074a7c0
Guenther Koessler <guenther.koessler@imendo.at>
2018-08-11 22:41:27 +0200

    implemented parts of the player

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ShelfManager.Instance.SelectLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ShelfManager.Instance.SelectRight();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int removeAmount = 100;
+
+            Pile.Instance.Subtract(removeAmount);
+            var addAmount = ShelfManager.Instance.Add(removeAmount);
+            if (addAmount > 0)
+            {
+                Pile.Instance.Add(addAmount);
+            }
+        }
+    }
+}
