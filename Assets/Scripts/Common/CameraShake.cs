71a1de88876a97948ba23683327825ea4a94de67
Fabian Schober <fabian@imendo.at>
2018-08-11 22:38:27 +0200

    Helper Scripts

---
+++ Assets/Scripts/Common/CameraShake.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+
+// Courtesy of https://gist.github.com/ftvs/5822103
+// Modified by us
+public class CameraShake : SceneSingleton<CameraShake>
+{
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    public float DefaultDuration = 1.5f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.3f;
+    public float decreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    void Awake()
+    {
+        SetInstance();
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+
+    public void Shake(float? duration = null)
+    {
+        duration = duration ?? DefaultDuration;
+        shakeDuration = duration.Value;
+    }
+}
---
+++ Assets/Scripts/Dragon/Dimmer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dimmer : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    public float TargetAlpha;
+    public float SmoothTime = 0.3f;
+
+    private float _alpha;
+
+    // Use this for initialization
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentAlpha = _spriteRenderer.color.a;
+        if (TargetAlpha != 0 && Math.Abs(TargetAlpha - currentAlpha) < 0.01)
+        {
+            TargetAlpha = 0;
+        }
+
+        float newAlpha = Mathf.SmoothDamp(currentAlpha, TargetAlpha, ref _alpha, SmoothTime);
+
+        _spriteRenderer.color = new Color(0, 0, 0, newAlpha);
+    }
+
+    public void Dim(float targetAlpha = 0.313f)
+    {
+        TargetAlpha = targetAlpha;
+    }
+}
