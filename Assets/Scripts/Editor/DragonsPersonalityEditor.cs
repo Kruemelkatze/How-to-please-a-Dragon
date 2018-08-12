e6e5ebb2db3e85278b1d033b5562140bcc137f20
Fabian Schober <fabian@imendo.at>
2018-08-12 12:50:11 +0200

    First DragonsPersonality Test

---
+++ Assets/Scripts/Editor/DragonsPersonalityEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DragonsPersonality))]
+public class DragonsPersonalityEditor : Editor
+{
+    private static int _amount = 200;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DragonsPersonality myScript = (DragonsPersonality) target;
+        EditorGUILayout.LabelField("Mood", myScript.Mood.ToString());
+    }
+}
