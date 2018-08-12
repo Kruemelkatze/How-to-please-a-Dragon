c88ffb08aefa07ea0011be7d0f4f8d942661a390
Fabian Schober <fabian@imendo.at>
2018-08-12 21:57:37 +0200

    LootManager skeleton

---
+++ Assets/Scripts/Loot/LootManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : SceneSingleton<LootManager>
+{
+    public Animation PanelAnimation;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ShowModal()
+    {
+        PanelAnimation.gameObject.SetActive(true);
+        PanelAnimation.Play("LootModal_in");
+        Debug.Log(PanelAnimation.GetClip("LootModal_in").length);
+    }
+
+    public void HideModal()
+    {
+        PanelAnimation.Play("LootModal_out");
+        Debug.Log(PanelAnimation.GetClip("LootModal_out").length);
+    }
+}
