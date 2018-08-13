59bb26b44574e83af1e94b69541b5b1d6d5f4356
Fabian Schober <fabian@imendo.at>
2018-08-13 15:31:26 +0200

    Added Scenes

---
+++ Assets/Scripts/SceneMangagement/DeathScreen.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class DeathScreen : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI TopScoreText;
+
+    private void Start()
+    {
+        if (ScoreText != null)
+        {
+            ScoreText.text = Highscore.LatestScore.ToString();
+        }
+
+        if (TopScoreText != null)
+        {
+            TopScoreText.text = Highscore.TopHighscore.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneMangagement/SceneTransition.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Prime31.TransitionKit;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+public class SceneTransition : MonoBehaviour
+{
+    public int NextSceneIndex = -1;
+
+    public bool Transitioning;
+    public float Duration = 0.7f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Transitioning && Input.GetKeyDown(KeyCode.Space))
+        {
+            Transitioning = true;
+            TransitionToScene(NextSceneIndex, Duration);
+        }
+    }
+
+    public static void TransitionToScene(int sceneIndex, float duration = 0.7f)
+    {
+//        if (AudioControl.Instance != null)
+//        {
+//            AudioControl.Instance.PlaySound("change_scene");
+//            Object value;
+//            if (AudioControl.Instance.SoundClips.TryGetValue("change_scene", out value))
+//            {
+//                var audioLength = ((AudioClip) value).length;
+//                duration = Math.Max(duration, audioLength);
+//            }
+//        }
+
+        if (sceneIndex < 0)
+        {
+            Debug.LogError("Scene unknown!");
+            return;
+        }
+
+        var transition = new Prime31.TransitionKit.FadeTransition()
+        {
+            nextScene = sceneIndex,
+            duration = duration,
+            fadeToColor = Color.black,
+        };
+
+        TransitionKit.instance.transitionWithDelegate(transition);
+    }
+}
