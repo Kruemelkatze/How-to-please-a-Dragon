7a47510161a4e18c73df06becfad650d161a403b
Guenther Koessler <guenther.koessler@imendo.at>
2018-08-13 11:58:06 +0200

    added shovel editor

--- Assets/Scripts/Shovel/ShovelScript.cs
+++ Assets/Scripts/Shovel/ShovelScript.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 public class ShovelScript : SceneSingleton<ShovelScript>
@@ -9,11 +10,15 @@ public class ShovelScript : SceneSingleton<ShovelScript>
     
     public int ShovelUpgrade = 0;
     public int[] AmountMap = new[] {50, 100, 200};
+    private Sprite[] SpriteMap = new Sprite[3];
     
     // Use this for initialization
     void Start()
     {
         setAmount();
+        SpriteMap[0] = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Player/char_shovel_up.png");
+        SpriteMap[1] = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Player/char_shovel_up_2.png");
+        SpriteMap[2] = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Player/char_shovel_up_3.png");
     }
 
     // Update is called once per frame
@@ -22,14 +27,14 @@ public class ShovelScript : SceneSingleton<ShovelScript>
         
     }
 
-    void UpgradeShovel(int level)
+    public void UpgradeShovel(int level)
     {
         level = Math.Max(level, ShovelUpgrade);
         ShovelUpgrade = level > 2 ? 2 : level;
         setAmount();
     }
 
-    void DowngradeShovel(int level)
+    public void DowngradeShovel(int level)
     {
         level = Math.Min(level, ShovelUpgrade);
         ShovelUpgrade = level < 0 ? 0 : level;
@@ -39,5 +44,6 @@ public class ShovelScript : SceneSingleton<ShovelScript>
     private void setAmount()
     {
         ShovelAmount = AmountMap[ShovelUpgrade];
+        Shovel.sprite = SpriteMap[ShovelUpgrade];
     }
 }
